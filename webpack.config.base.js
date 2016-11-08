af45e3ebbeef110c98be7d265d02e4bd8112d5e1
Andrew Romanov <andrey.romanov@rambler-co.ru>
2016-11-08 12:09:51 +0300

    Revert "don't compile styles using webpack"
    This reverts commit 2f101815e8bce6e34957dd00a9198f155af3953b.

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
 import glob from 'glob';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import autoprefixer from 'autoprefixer';
 
 
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
@@ -9,10 +11,11 @@ const SOURCE = './source';
 const COMPONENTS = `${SOURCE}/components`;
 const OUT = './lib';
 
+const extractStylus = new ExtractTextPlugin('[name].css');
 
 const componentsList = glob.sync(`${COMPONENTS}/*`).map(path => path.slice(COMPONENTS.length + 1));
 const componentsEntries = componentsList.reduce((acc, componentName) => {
-	acc[componentName] = [`${COMPONENTS}/${componentName}/index.jsx`];
+	acc[componentName] = [`${COMPONENTS}/${componentName}/index.jsx`, `${COMPONENTS}/${componentName}/index.styl`];
 	return acc;
 }, {});
 const entries = Object.assign({}, componentsEntries, {
@@ -47,11 +50,21 @@ module.exports = {
 				exclude: /(node_modules)/,
 				loader: 'babel',
 			},
+			{
+				test: /\.styl$/,
+				loader: extractStylus.extract('css!postcss!stylus'),
+			},
 		],
 	},
 	plugins: [
+		extractStylus,
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
 		}),
 	],
+	postcss: [
+	    autoprefixer({
+	      browsers: ['> 1%', 'last 5 versions'],
+	    }),
+	  ],
 };
